834675e98869a089454bd6ba4925dfcd88d4af89
Eric Eicke <eeicke@hotmail.com>
2023-02-04 14:32:05 -0500

    Moved comment

--- src/kernel/display.c
+++ src/kernel/display.c
@@ -1,3 +1,8 @@
+//Text Mode 16 Colors:
+//black(0) blue(1) green(2) cyan(3) red(4) magenta(5) brown(6) light gray(7) dark gray(8)
+//light blue(9) light green(10) light cyan(11) light red(12) light megenta(13) yellow(14) 
+//white(15)
+
 #define NULL 0
 
 unsigned char * VIDEOBUFFER = (unsigned char *)0xb8000;
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,8 +1,3 @@
-//Text Mode 16 Colors:
-//black(0) blue(1) green(2) cyan(3) red(4) magenta(5) brown(6) light gray(7) dark gray(8)
-//light blue(9) light green(10) light cyan(11) light red(12) light megenta(13) yellow(14) 
-//white(15)
-
 extern void kprintWithColor(char *, unsigned char);
 extern void kprint(char *);
 
