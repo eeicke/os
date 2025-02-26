89b1f23c3b5114133ab3b8c44223269c55fe7c6d
Eric Eicke <eeicke@hotmail.com>
2025-02-26 16:56:12 -0500

    Remove

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -9,23 +9,6 @@ void kernel_main()
 {         
     kprint("Starting OS\n");     
     
-    // uint8_t *buf = (uint8_t *)0x100000;
-
-    // for (int i = 0; i < 512; ++i)
-    //     buf[i] = '\0';
-
-
-    // buf[0] = 'D';
-    // buf[1] = 'u';
-    // buf[2] = 'm';
-    // buf[3] = 'p';
-    // buf[4] = 'y';
-
-    // if (WriteBlockPIO(0, 2048, buf) < 0)
-    //     kprint("Failed\n");
-    // else
-    //     kprint("OK\n");
-    
     while( 1 );
 }
 
