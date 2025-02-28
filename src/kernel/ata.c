375b42afb0004688042a2a6f5a47d1228da65df9
Eric Eicke <eeicke@hotmail.com>
2025-02-28 15:06:45 -0500

    remove spaces

--- src/kernel/ata.c
+++ src/kernel/ata.c
@@ -8,8 +8,8 @@ int ReadBlockPIO(uint8_t drive, uint32_t lba, void* buffer)
     OutByte(BASEPORT + 6, ((0x0e0 | (drive << 4)) | ((lba & 0x0F000000 ) >> 24)));            
     OutByte(BASEPORT + 2, 1);
     OutByte(BASEPORT + 3, lba & 0x000000FF);
-    OutByte(BASEPORT + 4, (lba & 0x0000FF00 ) >> 8);
-    OutByte(BASEPORT + 5, (lba & 0x00FF0000 ) >> 16);
+    OutByte(BASEPORT + 4, (lba & 0x0000FF00) >> 8);
+    OutByte(BASEPORT + 5, (lba & 0x00FF0000) >> 16);
     OutByte(BASEPORT + 7, 0x20); 
 
     if (WaitForDriveReady() < 0)
